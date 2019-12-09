5702d7c6f0c76e177a8226becb047f54c8d4b12d
Henry <hernysleek51@gmail.com>
2019-12-09 11:23:44 -0800

    day 2 web ui

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <!--Form Back Drop-->
+    <div class="form-back-drop"></div>
+
+   <!-- banner-style-two -->
+    <section class="banner-style-two" style="background-image: url(images/background/banner-3.png);"> 
+        <div class="large-container clearfix">
+            <div class="inner-box">
+                <div class="row">
+                    <div class="col-lg-6 col-md-12 col-sm-12 image-column">
+                        <div class="image-box clearfix">
+                            <figure class="image-one"><img src="/images/icons/icon-1.png" alt=""></figure>
+                            <figure class="image-two"><img src="/images/resource/banner-img-2.png" class="flot-bob-y" alt=""></figure>
+                            <figure class="image-three paroller"><img src="/images/resource/banner-img-3.png"></figure>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-12 col-sm-12 content-column">
+                        <div class="content-box">
+                            <h1>Improve your schooling and never miss a schedule<span>How?</span></h1>
+                            <div class="text">We help in reminding, scheduling, and calling when late <br> catapulting you and your class ahead of the competition</div>
+                            <div class="btn-box"><router-link to="/login">Login</router-link></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- banner-style-two end -->
+
+        
+ 
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld,
-  },
+  name: "home",
 };
 </script>
