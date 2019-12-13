8fbb8dd196bb5fd7a05c5391eeb7ff744e848400
MartinsOnuoha <martinsvictor.nd@gmail.com>
2019-12-13 20:23:12 +0100

    finish nexmo integration

--- server/routes/api.php
+++ server/routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/twilio/sms/send', 'TwilioController@sendSms');
+Route::post('/nexmo/call', 'TwilioController@call');
